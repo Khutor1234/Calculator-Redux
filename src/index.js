2cf125be5d7d7a5497fb468579023d90a2fbd96e
Alexandra Khutorna <khutornaalexandra@gmail.com>
2021-02-02 16:20:15 +0200

    Optimization

--- src/index.js
+++ src/index.js
@@ -1,38 +1,22 @@
-import {createStore} from "redux";
+import {createStore, bindActionCreators} from "redux";
 import reducer from './reducer';
-import {inc,dec,res,rnd} from './actions';
+import * as actions from './actions';
 
 const store = createStore(reducer);
 const {dispatch} = store;
 
-const incDispatch = () => dispatch(inc());
-const decDispatch = () => dispatch(dec());
-const rndDispatch = (value) => dispatch(rnd(value));
-const resDispatch = () => dispatch(res());
+const {inc, dec, rnd, res} = bindActionCreators(actions,dispatch); // на выходе обернутые в dispatch
 
-document.getElementById('inc').addEventListener('click', incDispatch);
-document.getElementById('dec').addEventListener('click', decDispatch);
-document.getElementById('res').addEventListener('click', resDispatch);
+document.getElementById('inc').addEventListener('click', inc);
+document.getElementById('dec').addEventListener('click', dec);
+document.getElementById('res').addEventListener('click', res);
 document.getElementById('rnd').addEventListener('click', () => {
     const value = Math.floor(Math.random() * 10);
-    rndDispatch(value)
+    rnd(value)
 });
 
 const update = () => {
     document.getElementById('counter').textContent = store.getState();
 }
 
-store.subscribe(update)
-
-// store.dispatch({type: 'INK'});
-// store.dispatch({type: 'DEC'});
-// store.dispatch({type: 'INK'});
-
-
-// let state = reducer(undefined, {});
-
-// state = reducer(state, {type: 'INK'});
-// console.log(state);
-// state = reducer(state, {type: 'INK'});
-// console.log(state);
-
+store.subscribe(update);
